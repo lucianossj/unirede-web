fe4d38b19a6986c0677be9ddb1d5bcd4f78d90d8
Luciano S. Junior <luciano.ssj@hotmail.com>
2018-10-13 17:55:50 -0300

    Users' Register screen front-end done. / Front-end da tela do Cadastro de Usuários completo.

---
+++ unirede-web/src/app/users-register/users-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'users-register',
+  templateUrl: './users-register.component.html',
+  styleUrls: ['./users-register.component.css']
+})
+export class UsersRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
